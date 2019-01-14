06920f1ad4b761aead217dcd30970ac13a4e849c
jebuth <justinbuth91@live.com>
2019-01-13 17:59:23 -0800

    initial commit

---
+++ components/layouts/BaseLayout.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import Header from '../shared/Header';
+
+const BaseLayout = (props) => {
+
+	return (
+		<React.Fragment>
+		<Header />
+			{props.children}
+		</React.Fragment>
+	)
+}
+
+export default BaseLayout;
---
+++ components/shared/Header.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Link from 'next/link';
+import '../../styles/main.scss';
+//import BaseLayout from '../components/layouts/BaseLayout'
+
+class Header extends React.Component{
+
+	render(){
+	
+		const title = this.props.title;
+
+		return (
+			<React.Fragment>
+
+				<p> { title } </p>
+				{this.props.children}
+
+				<p className='customClass'> I am styled P element </p>
+				<p className='customClassFromFile'> I am styled P element </p>
+
+				<Link href="/">
+					<a> Home </a>
+				</Link>
+
+				<Link href="/about">
+					<a> About </a>
+				</Link>
+
+				<Link href="/portfolios">
+					<a> Portfolio </a>
+				</Link>
+
+				<Link href="/blogs">
+					<a> Blog</a>
+				</Link>
+
+				<Link href="/cv">
+					<a> CV </a>
+				</Link>
+
+				<style jsx>
+					{
+					`
+						a {
+							font-size: 20px;
+						};
+
+						.customClass{
+							color: red;
+						}
+					`
+					}
+				</style>
+
+			</React.Fragment>
+		)
+	}
+}
+
+export default Header;
