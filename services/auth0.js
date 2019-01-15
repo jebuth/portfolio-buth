e0292a6f00cdd1ebd63225267344ca2e2ece1608
jebuth <justinbuth91@live.com>
2019-01-14 22:22:28 -0800

    saved auth result to cookies

--- services/auth0.js
+++ services/auth0.js
@@ -1,4 +1,5 @@
 import auth0 from 'auth0-js';
+import Cookies from 'js-cookie';
 
 class Auth0 {
 
@@ -14,6 +15,35 @@ class Auth0 {
     // bind the context of login function to this class.
     // login() is called from Login component in Header which uses a different context.
     this.login = this.login.bind(this);
+
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+  }
+
+  handleAuthentication() {
+
+    return new Promise((resolve, reject) => {
+        this.auth0.parseHash((err, authResult) => {
+          if (authResult && authResult.accessToken && authResult.idToken) {
+            this.setSession(authResult);
+            resolve();
+
+          } else if (err) {
+            reject();
+            console.log(err);
+          }
+      });
+    })
+  }
+
+  setSession(authResult) {
+
+    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    
+    Cookies.set('user', authResult.idTokenPlayload);
+    Cookies.set('jwt', authResult.idToken);
+    Cookies.set('expires-at', expiresAt);
+
+
   }
 
   login() {
